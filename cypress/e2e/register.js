46bfa10e93ae3f907b3b043e69d439228ee94efb
liznyamu <liznyamu@gmail.com>
2024-10-11 15:31:44 +0300

    removed login tests from register test suite

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -2,11 +2,12 @@
 import { faker } from '@faker-js/faker';
 
 describe('Register and Login Tests - e2e tests', () => {
+    //TODO: use page-object and elements
     let email = faker.internet.email();
     let password = faker.internet.password();
 
     it('Validate user can Register an new account', () => {
-        cy.visit('/');
+        cy.visit('/register');
 
         // close Welcome to OWASP popup
         cy.get('.cdk-overlay-backdrop').click(-50, -50, { force: true });
@@ -28,9 +29,9 @@ describe('Register and Login Tests - e2e tests', () => {
         cy.get('#securityAnswerControl').type(faker.person.fullName());
         cy.get('#registerButton').click();
 
-        cy.wait('@createUser').its('response.statusCode').should('eq', 201);
-        cy.get('@createUser').its('response.body.data.email').should('eq', email);
-        cy.get('@createUser').then(({ response }) => {
+        // validate user is created
+        cy.wait('@createUser').then(({ response }) => {
+            expecte(response.statusCode).to.eq(200);
             expect(response.body.data.email).to.equal(email);
             expect(response.body.data).property('email').to.equal(email);
         })
@@ -44,32 +45,5 @@ describe('Register and Login Tests - e2e tests', () => {
 
     });
 
-    it('Validate user can sucessfully Login', () => {
-        // TODO use api requests to register a new user 
-        cy.visit('/');
-
-        cy.get('.cdk-overlay-backdrop').click(-50, -50, { force: true });
-
-        cy.get('#navbarAccount').click();
-        cy.get('#navbarLoginButton').click();
-
-        cy.intercept('POST', '**/user/login').as('userLogin');
-        cy.get('#email').type(email);
-        cy.get('#password').type(password);
-        cy.get('#loginButton').click();
-
-        cy.wait('@userLogin').then(({ response }) => {
-            expect(response.statusCode).to.eq(200);
-            let accessToken = response.body.authentication.token;
-            cy.wrap(response.body.authentication.token).as('accessToken');
-            cy.log('accessToken - first' + accessToken);
-        }).then(function () {
-            let accessToken = this.accessToken;
-            cy.log('accessToken - again\n' + accessToken)
-        });
-
-        // navbar top >> cart 
-        cy.get('.mat-toolbar-row').should('contain', '0');
-
-    });
 });
+
